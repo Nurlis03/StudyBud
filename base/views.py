f976b1b421b04522b8903363f7fe98d5e13be463
Nurlis <kimbiletovnurlis03@gmail.com>
2023-11-28 16:41:18 +0600

    initialize project

--- base/views.py
+++ base/views.py
@@ -18,26 +18,20 @@ from .forms import RoomForm, UserForm, MyUserCreationForm
 
 def loginPage(request):
     page = 'login'
-    if request.user.is_authenticated:
-        return redirect('home')
-
     if request.method == 'POST':
-        email = request.POST.get('email').lower()
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
 
         try:
-            user = User.objects.get(email=email)
-        except:
+            user = User.objects.get(username=username)
+        except:  # noqa: E722
             messages.error(request, 'User does not exist')
-
-        user = authenticate(request, email=email, password=password)
-
+        user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
             return redirect('home')
         else:
-            messages.error(request, 'Username OR password does not exit')
-
+            messages.error(request, 'Username Or password does not exit')
     context = {'page': page}
     return render(request, 'base/login_register.html', context)
 
